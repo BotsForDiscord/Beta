13a9a3740060b66a9ab38bc382824a151b898861
Dev!ne <mredatedev@gmail.com>
2019-01-02 00:13:22 -0500

    Really sloppy, will clean when im not tired af, somewhat responsive im working on it.

---
+++ source/assets/homepage.js
@@ -0,0 +1,6 @@
+function dropdown (that) {
+    if (that.children[1].style.display !== 'block')
+    that.children[1].style.display = "block";
+    else 
+    that.children[1].style.display = 'none';
+}
